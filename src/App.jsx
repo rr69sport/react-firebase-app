9ce232eb94decae7a174dd937f43ba674bc6b508
rr69sport <richardramsua@gmail.com>
2022-07-13 03:58:05 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <>
+      <h1 className={styles.title}>React with Firebase App</h1>
+      <p className={styles.text}>React 18 Firebase 9</p>
+    </>
+  );
+};
+
+export default App;
